657c5985cfb80485c5034e47c9221ce3411a2b9a
Chorusmaster <max.mamkin.experementer@gmail.com>
2025-04-30 10:53:42 +0200

    riešenie konfliktov 4

--- parts/header.php
+++ parts/header.php
@@ -20,14 +20,3 @@
         }
     ?>
 </header>
-    <div class="banner">
-        <h1>ŽBANKA</h1>
-        <?php
-        if (!include("php/citations.php")) {
-            echo "<em>Ooops, citácie nie sú k dispozícii</em>";
-        }
-        ?>
-    </div>
-</header>
-
-<hr class="hr_decoration">
